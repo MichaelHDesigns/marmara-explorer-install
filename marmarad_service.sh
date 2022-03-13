982d62c5d14af456147124661aee53cacc902cb9
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-03-13 01:50:10 -0500

    Update marmarad_service.sh

--- marmarad_service.sh
+++ marmarad_service.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 CUR_DIR=$(pwd)
-cat <<EOF >$HOME/marmarad.service
+cat <<EOF >$HOME/helpthehomelessd.service
 [Unit]
-Description=komodo daemon for MCL chain
+Description=helpthehomeless coin daemon for MCL chain
 
 [Service]
-WorkingDirectory=$HOME/komodo/src/
-ExecStart=$HOME/komodo/src/komodod -ac_name=MCL -ac_supply=2000000 -ac_cc=2 -addnode=37.148.210.158 -addnode=37.148.212.36 -addnode=149.202.158.145 -txindex=1 -addressindex=1 -spentindex=1 -timestampindex=1 -ac_marmara=1 -ac_staked=75 -ac_reward=3000000000
-ExecStop=$HOME/komodo/src/komodo-cli -ac_name=MCL stop
+WorkingDirectory=$HOME/helpthehomelesscoin/src/
+ExecStart=$HOME/helpthehomelesscoin/src/helpthehomelessd -ac_name=HTH -ac_supply=2000000 -ac_cc=2 -addnode=73.150.182.108 -addnode=155.138.198.71 -addnode=67.166.253.246 -txindex=1 -addressindex=1 -spentindex=1 -timestampindex=1 -ac_marmara=1 -ac_staked=75 -ac_reward=3000000000
+ExecStop=$HOME/helpthehomelesscoin/src/helpthehomeless-cli -ac_name=HTH stop
 User=$USER
 Restart=on-failure
 TimeoutSec=300
@@ -19,8 +19,8 @@ EOF
 cat <<EOF >$HOME/explorer-marmara.service
 [Unit]
 Description=marmara explorer service
-Requires=marmarad.service
-After=marmarad.service
+Requires=helpthehomelessd.service
+After=helpthehomelessd.service
 
 [Service]
 WorkingDirectory= $CUR_DIR/
